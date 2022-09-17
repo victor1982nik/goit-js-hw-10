cb1810e8756f8ca9135ce2ba0c270322293ee43d
Victor Nikolenko <victor1982nik@gmail.com>
2022-09-17 15:17:40 +0300

    first issue working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import { debounce, result } from 'lodash'
 
 const DEBOUNCE_DELAY = 300;
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+const inputRef = document.querySelector("#search-box");
+const listRef = document.querySelector(".country-list");
+const countryRef = document.querySelector(".country-info");
+
+//Notiflix.Notify.success(inputRef);
+inputRef.addEventListener('input', debounce(handleInput,DEBOUNCE_DELAY));
+//inputRef.addEventListener('input', handleInput);
+
+
+function fetchCountries(name) {
+    const url = BASE_URL + `${name}?fields=name,population,capital,flags,languages`;
+    
+    return fetch(url)
+        .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }        
+        return response.json();
+    })        
+        .then(data => {
+            if (data.length > 10) {
+                Notiflix.Notify.warning("Too many matches found. Please enter a more specific name.");
+                return;
+            }
+            if (data.length < 10 && data.length > 2) {
+                renderList(data);
+                return;
+            }
+            
+            renderOne(data);
+    })
+    .catch (error => {       
+        Notiflix.Notify.failure("Oops, there is no country with that name");
+  });
+}
+
+function handleInput(e) {
+    e.preventDefault();
+    listRef.innerHTML = '';
+    
+    const countryName = e.target.value.trim();
+    if (!countryName.length)         
+        return;    
+          
+    fetchCountries(countryName);    
+}
+
+function renderList(countries) {
+    let markup = '';    
+           
+    markup = countries.map(({ flags, name }) => {
+        return `<li><img src="${flags.svg}"<span>  ${name.official}</span></li>`
+    }).join("");        
+   
+    listRef.insertAdjacentHTML('afterbegin',markup);
+}
+
+function renderOne(country) {
+    let markup = '';
+    const { flags, name, capital, population, languages } = country[0];
+        
+    markup = `<img src="${flags.svg}"><span class='header'> ${name.official}</span>
+       <p><b>Capital: </b>${capital}</p><p><b>Population: </b>${population}</p>
+        <p><b>Languages: </b>${Object.values(languages)}</p>`;
+    
+    listRef.insertAdjacentHTML('afterbegin',markup);
+}
